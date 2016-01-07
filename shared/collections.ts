955aafc298e0380f74aeb26c689e9b1e0775297d
Elena <lena@soapyfrog.com>
2016-01-07 02:07:35 +0000

    typescript migration work in progress

--- shared/collections.ts
+++ shared/collections.ts
@@ -1,10 +1,17 @@
 /// <reference path="../typings/meteor/meteor.d.ts" />
-
 interface IChats{
     _id?: string;
     name: string;
     status?: string;
     queuedAt?: string;
+    messages?:Array<Object>;
+}
+
+interface IChatMessage{
+    _id?: string;
+    userId:string;
+    text:string;
+    timestamp:Date;
 }
 
 declare var Chats: Mongo.Collection<IChats>;
